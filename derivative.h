9ef803d6532838a883e2456f40122b9548eb0241
iluzin <isluzin@gmail.com>
2024-01-01 21:08:25 +0300

    Create derivative.h

---
+++ derivative.h
@@ -0,0 +1,12 @@
+#ifndef __DERIVATIVE_H__
+#define __DERIVATIVE_H__
+
+#include <string>
+#include <vector>
+
+std::string derivative(std::string);
+int power(int, unsigned int);
+void replace(std::string &, const std::string &, const std::string &);
+std::vector<std::string> split(const std::string &, const std::string &);
+
+#endif
